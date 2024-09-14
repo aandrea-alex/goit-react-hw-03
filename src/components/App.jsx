0132079df70f5471d315c462045142b3db7c4fb5
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-15 00:05:21 +0200

    initial commit for Module 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import userData from '../data/userData.json';
-import friendsData from '../data/friendsData.json';
-import transactions from '../data/transactions.json';
-import { Profile } from './Profile/Profile';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './Transactions/TransactionHistory';
 
 function App() {
-  const { username, tag, location, avatar, stats } = userData;
   return (
     <React.Fragment>
-      <Profile
-        name={username}
-        tag={tag}
-        location={location}
-        image={avatar}
-        stats={stats}
-      />
-      <FriendList friends={friendsData} />
-      <TransactionHistory items={transactions} />
+      <p >Home 3</p>
     </React.Fragment>
   );
 }
