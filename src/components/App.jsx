7138ec6863afe3a7ddcdf24a643e9c321476ea96
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-15 12:06:29 +0200

    styles changed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { nanoid } from 'nanoid';
 import { useState, useEffect } from 'react';
 import TitleSection from './TitleSection/TitleSection';
 import ContactForm from './ContactForm/ContactForm';
 import { TITLE } from '../js/constants';
-import { getContactsList } from "../js/localstorage/getContactsList";
-import { saveContactsList } from "../js/localstorage/saveContactsList";
+import { getContactsList } from '../js/localstorage/getContactsList';
+import { saveContactsList } from '../js/localstorage/saveContactsList';
+import styles from './App.module.css';
 
 function App() {
   const [contacts, setContacts] = useState(getContactsList);
@@ -22,10 +22,10 @@ function App() {
   }, [contacts]);
 
   return (
-    <React.Fragment>
+    <div className={styles.container}>
       <TitleSection>{TITLE}</TitleSection>
       <ContactForm onAddContact={handleAddContact} />
-    </React.Fragment>
+    </div>
   );
 }
 
